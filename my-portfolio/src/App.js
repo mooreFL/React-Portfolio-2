184c6ffec78ba707f7766230701bfe4e072cfed1
mooreFL <moorepr09@gmail.com>
2021-07-11 11:47:37 -0400

    initial files added

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components.js/Navbar";
+import Projects from "./components.js/Projects";
+import Skills from "./components.js/Skills";
+import Testimonials from "./components.js/Testimonials";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
